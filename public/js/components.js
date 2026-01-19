83fe6515964f73237c5b93d0d36bf8cdf22bcded
mkt9834 <mkt9834@gmail.com>
2026-01-20 01:10:03 +0900

    Fix: 사이트보기 링크를 홈페이지(/)로 수정

--- public/js/components.js
+++ public/js/components.js
@@ -75,7 +75,7 @@ function loadSidebar(currentPage) {
     </nav>
 
     <div class="sidebar-footer">
-      <a href="../index.html" class="nav-item">
+      <a href="/" class="nav-item">
         <span class="nav-icon">
           <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
             <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/>
